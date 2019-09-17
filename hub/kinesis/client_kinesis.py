2d9523052702c1a0d25c61271c809312307f233b
rogelioLpz <rogelio.lpz94@gmail.com>
2019-09-17 10:19:26 -0500

    Renombrar variables archivo .env

--- hub/kinesis/client_kinesis.py
+++ hub/kinesis/client_kinesis.py
@@ -2,8 +2,8 @@ import os
 
 import boto3
 
-ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
-SECRET_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+ACCESS_KEY = os.environ['ACCESS_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 REGION_NAME = os.environ['AWS_DEFAULT_REGION']
 
 client = boto3.client('kinesis', region_name=REGION_NAME,
