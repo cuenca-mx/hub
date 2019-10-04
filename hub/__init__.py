627b6f1e2b637acfd5990feb4b767ecb7d5f45c8
Ricardo Sánchez <rsc@cuenca.com>
2019-10-04 14:49:54 -0500

    Move client to init and add typing

--- hub/__init__.py
+++ hub/__init__.py
@@ -2,6 +2,28 @@ import types
 
 from hub.workers.worker import Worker
 
+import os
+
+import boto3
+
+ACCESS_KEY = os.environ['ACCESS_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
+REGION_NAME = os.environ['REGION_NAME']
+
+client = boto3.client(
+    'kinesis',
+    region_name=REGION_NAME,
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY,
+)
+
+dynamo_client = boto3.client(
+    'dynamodb',
+    region_name=REGION_NAME,
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY,
+)
+
 
 def init_workers(module, num_workers):
     """
