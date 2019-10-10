0e22f64d6f455a6ddad448b924cb1f3aa4420d2a
rogelioLpz <rogelio.lpz94@gmail.com>
2019-10-09 19:25:27 -0500

    Crear tabla dynamo si no existe

--- hub/db/dynamo.py
+++ hub/db/dynamo.py
@@ -8,6 +8,19 @@ KINESIS_TTL_HOURS = os.getenv('KINESIS_TTL_HOURS', 24)
 
 
 def write_to_db(key: str) -> bool:
+    # Create if nos exists
+    try:
+        response = dynamo_client.describe_table(TableName=KINESIS_DYNAMO_TABLE)
+    except dynamo_client.exceptions.ResourceNotFoundException:
+        dynamo_client.create_table(
+            TableName=KINESIS_DYNAMO_TABLE,
+            KeySchema=[{'AttributeName': 'uuid', 'KeyType': 'HASH'}],
+            AttributeDefinitions=[
+                {'AttributeName': 'uuid', 'AttributeType': 'S'},
+                {'AttributeName': 'ttl', 'AttributeType': 'N'},
+            ],
+        )
+
     ttl = int(time.time() + float(KINESIS_TTL_HOURS) * 3600)
     response = dynamo_client.put_item(
         TableName=KINESIS_DYNAMO_TABLE,
