d11806babf0a9b81d2bd9adf3156ba3cf7cdc024
rogelioLpz <rogelio.lpz94@gmail.com>
2019-10-09 20:07:49 -0500

    Tabla dynamo solo se crea si no existe al insertar

--- hub/db/dynamo.py
+++ hub/db/dynamo.py
@@ -8,19 +8,25 @@ KINESIS_TTL_HOURS = os.getenv('KINESIS_TTL_HOURS', 24)
 
 
 def write_to_db(key: str) -> bool:
-    # Create if nos exists
     try:
-        response = dynamo_client.describe_table(TableName=KINESIS_DYNAMO_TABLE)
-    except dynamo_client.exceptions.ResourceNotFoundException:
-        dynamo_client.create_table(
-            TableName=KINESIS_DYNAMO_TABLE,
-            KeySchema=[{'AttributeName': 'uuid', 'KeyType': 'HASH'}],
-            AttributeDefinitions=[
-                {'AttributeName': 'uuid', 'AttributeType': 'S'},
-                {'AttributeName': 'ttl', 'AttributeType': 'N'},
-            ],
-        )
+        return insert_register(key)
+    except ValueError:
+        create_table()
+        return insert_register(key)
 
+
+def create_table():
+    dynamo_client.create_table(
+        TableName=KINESIS_DYNAMO_TABLE,
+        KeySchema=[{'AttributeName': 'uuid', 'KeyType': 'HASH'}],
+        AttributeDefinitions=[
+            {'AttributeName': 'uuid', 'AttributeType': 'S'},
+            {'AttributeName': 'ttl', 'AttributeType': 'N'},
+        ],
+    )
+
+
+def insert_register(key: str) -> bool:
     ttl = int(time.time() + float(KINESIS_TTL_HOURS) * 3600)
     response = dynamo_client.put_item(
         TableName=KINESIS_DYNAMO_TABLE,
