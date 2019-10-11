0fc529f34bd75992e0000e4ccd3622c40da59a24
rogelioLpz <rogelio.lpz94@gmail.com>
2019-10-10 20:59:56 -0500

    Refactor decorador

--- hub/kinesis/decorators.py
+++ hub/kinesis/decorators.py
@@ -1,20 +1,7 @@
-from hub.db.dynamo import write_to_db
-from hub.kinesis.data_kinesis import DataKinesis
-
-
 def hub_task(stream: str):
     def decorator(function):
         def proccess_record(record):
-            unique_trans = write_to_db(record.get("uuid"))
-            if unique_trans:
-                r_body = function(record['body'])
-                res = DataKinesis(
-                    uuid=record.get("uuid"),
-                    task=record.get("task"),
-                    body=r_body,
-                    headers=dict(),
-                )
-                return res.to_dict()
+            return function(record)
 
         setattr(proccess_record, "hub_task", stream)
         return proccess_record
